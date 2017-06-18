a23741f382aed4fee93e067e9561d7b2d9597ad9
shoux28 <shoux28@gmail.com>
2017-06-18 16:53:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/HelloWorldActivity.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloWorldActivity extends AppCompatActivity {
+    private static final String TAG = "HelloWorldActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+        Log.d(TAG, "onCreate: execute");
+    }
+}
